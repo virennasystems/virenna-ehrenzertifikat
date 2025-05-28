cf4919f6c6a6347a29c0ebfdbed865f6c78bd3ad
virennasystems <rosen_notizbuch0e@icloud.com>
2025-05-28 20:52:19 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,13 @@
-const CACHE_NAME = 'ehrenzertifikat-v2';
+const CACHE_NAME = 'ehrenzertifikat-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/offline.html',
-  '/VIRENNA_Siegel.PNG',
-  '/favicon-32x32.png',
-  '/apple-touch-icon.png',
-  '/site.webmanifest'
+  './',
+  './index.html',
+  './favicon-32x32.png',
+  './VIRENNA_Siegel.PNG',
+  './site.webmanifest',
+  './apple-touch-icon.png',
+  './manifest.json',
+  './offline.html'
 ];
 
 self.addEventListener('install', event => {
@@ -18,27 +19,22 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() =>
-        caches.match('/offline.html')
-      )
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then(response => response || fetch(event.request))
-    );
-  }
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request).catch(() => caches.match('./offline.html'))
+    )
+  );
 });
